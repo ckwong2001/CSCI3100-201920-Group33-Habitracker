a927c763ce17697e4a85232d0844310d9550a16f
ckwong2001 <62646998+ckwong2001@users.noreply.github.com>
2020-05-17 03:50:54 +0800

    commented code

--- weekly_report.php
+++ weekly_report.php
@@ -1,17 +1,11 @@
-<?php
-    //to prevent sending emails to a lot of accounts, i just send this to my personal acc
-    //just paste the whole sending thing back into the for loop then will send to each user who ticked weekly report
-    //have a nice day and happy coding
-    //dont forget to delete these
-    ?>
-
+//automated using crontab on Mac
 <?php
     require 'db_key.php';
     require('fpdf182/fpdf.php');
     session_start();
     $connString = connect_db();
     $connect = new PDO("mysql:host=localhost;dbname=habitracker", "root", "");
-    
+    //using FPDF to generate the PDF
     class PDF extends FPDF
     {
         // Page header
@@ -42,8 +36,7 @@
     
     $lastSunday = date('Y-m-d',strtotime('last sunday'));
     $dayafterlastSunday = date('Y-m-d',strtotime('last sunday -6 days'));
-    
-    //$query = "SELECT * FROM login where receive_weeklyreport = '0'";
+    //retrieve the information of all users
     $query = "SELECT * FROM login";
     
     $statement = $connect->prepare($query);
@@ -51,7 +44,7 @@
     $statement->execute();
     
     $result = $statement->fetchAll();
-    
+    //handle the results user by user
     foreach($result as $row) {
         $username = $row['username'];
         
@@ -75,7 +68,7 @@
         $pdf->Cell(55,6,"Streaks as of $lastSunday",1,1);
         //the first number add up to 189 = a line
         
-    
+    //get the goals that were on progress in the previous week
         $query = "SELECT * FROM goals WHERE username = '$username' and goal_enddate >= '$dayafterlastSunday'";
         $statement = $connect->prepare($query);
         $statement->execute();
@@ -84,21 +77,19 @@
         foreach($result_2 as $row_2){
             $streak = $row_2['streak'];
             $goal_id = $row_2['goal_id'];
+            //store the streaks in the database
             mysqli_query($connString, "Update goals Set streak_lastSun = '$streak' Where goal_id = '$goal_id'");
+            //display the goal details
             $pdf->Cell(40,10,$row_2['goal_id'],1,0);
             $pdf->Cell(94,10,$row_2['goal_name'],1,0);
             $pdf->Cell(55,10,$row_2['streak_lastSun'],1,1);
         }
-        
+        //send the email to users who requested receiving weekly report via email
         if ($row['receive_weeklyreport']==1) {
-        //$pdf->output(); 
         $to = $row['email'];
-        //$to = 'wongchika@ymail.com';
         
-        //$file_name = 'testing_report' . '.pdf';
         $pdfoutputfile = 'C:\xampp\htdocs\habitracker\img\temp-file.pdf';
         $file = $pdf->Output($pdfoutputfile,'F');
-        //$file = $pdf->Output();
         $attachment = chunk_split(base64_encode($file));
 
 
@@ -129,14 +120,11 @@
         $mail->isHTML();
         $mail->Username = 'noreply.habitracker@gmail.com';//your own email address
         $mail->Password = 'csci3100';
-        
-        //$mail->Subject = $subject;
-        //$mail->Body = $message;
+
         $mail->AddAddress($to);
         
         $mail->AddEmbeddedImage('img/logo.png','logo');
         $mail->AddAttachment('C:\xampp\htdocs\habitracker\img\temp-file.pdf',$name = 'myattachment',$encoding = 'base64', $type ='application/pdf');
-        //$mail->addStringAttachment($attachment, 'myattachment.pdf');
         
         $mail->Subject = $subject;
         $mail->Body = $message;
