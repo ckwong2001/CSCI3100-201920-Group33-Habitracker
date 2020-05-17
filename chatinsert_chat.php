ea6394712b9de34bf4e99c57859018e02814db82
ckwong2001 <62646998+ckwong2001@users.noreply.github.com>
2020-05-17 10:27:26 +0800

    Update chatinsert_chat.php

--- chatinsert_chat.php
+++ chatinsert_chat.php
@@ -5,12 +5,14 @@ include ('chatdatabase_connection.php');
 session_start();
 
 $data = array(
-    ':to_user_id'  => $_POST['to_user_id'],  //person receive message
-    ':from_user_id'  => $_SESSION['user_id'],  //person send message
+    ':to_user_id'  => $_POST['to_user_id'],  //person who receives a message
+    ':from_user_id'  => $_SESSION['user_id'],  //person who sent a message
     ':chat_message'  => $_POST['chat_message'],
     ':status'   => '1'
    );
 
+//query for inserting chat message and its related information such as sender, receiver, 
+// and status (whether a message is read) into the database 
 $query = "
 INSERT INTO chat_message 
 (to_user_id, from_user_id, chat_message, status) 
