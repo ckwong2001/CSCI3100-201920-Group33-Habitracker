0899180003eee9cffe4830cab34131c5700227a5
ckwong2001 <62646998+ckwong2001@users.noreply.github.com>
2020-05-17 13:04:38 +0800

    Update signup.inc.php

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -1,13 +1,18 @@
+<!--backend code when one submit a signup request-->
+
 <?php
 if (isset($_POST['signup-submit'])){
 
     require 'dbh.inc.php';
 
+   //allocate variables to store users' input in the frontend page
     $username = $_POST['uid'];
     $email = $_POST['mail'];
     $password = $_POST['pwd'];
     $passwordRepeat = $_POST['pwd-repeat'];
 
+    //check the validity of users' input and redirect users back to the frontend page with url corresponding to their invalid input
+    //for display of error message by the frontend page 
     if(empty($username) || empty($email) || empty($password) || empty($passwordRepeat)) {
         header("Location: ../signup.php?error=emptyfields&uid=".$username."&mail=".$email);
         exit();
@@ -30,13 +35,15 @@ if (isset($_POST['signup-submit'])){
         exit();
     }
     else {
-
+        
+//retrieve data from the database to check the validity of users' input in the signup form
         $sql = "SELECT username FROM login WHERE username=?";
         $stmt = mysqli_stmt_init($conn);
         if (!mysqli_stmt_prepare($stmt, $sql)){
             header("Location: ../signup.php?error=sqlerror");
             exit();
         }
+        
         else {
             mysqli_stmt_bind_param($stmt, "s", $username);
             mysqli_stmt_execute($stmt);
@@ -54,10 +61,12 @@ if (isset($_POST['signup-submit'])){
                     exit();
                 }
                 else {
+                    //check the password enter by user and update the database 
                     $hashedPwd = password_hash($password, PASSWORD_DEFAULT);
 
                     mysqli_stmt_bind_param($stmt, "sss", $username, $email, $hashedPwd);
                     mysqli_stmt_execute($stmt);
+                    //print success message to notify the user 
                     header("Location: ../signup.php?signup=success");
                     exit();
                 }
