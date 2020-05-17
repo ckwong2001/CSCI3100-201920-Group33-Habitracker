43e40bf850a2a7c07d5f727112796a6537dfdf3f
ckwong2001 <62646998+ckwong2001@users.noreply.github.com>
2020-05-17 17:26:45 +0800

    commented code

--- groupchat_fetch_user_chat_history.php
+++ groupchat_fetch_user_chat_history.php
@@ -1,12 +1,10 @@
 <?php
-//fetch the group chat history of a certain activity when a user open the group chat dialogue box
-
-//fetch_user_chat_history.php
-
-include('chatdatabase_connection.php');
-
-session_start(); //get session variable
-
-echo fetch_activity_chat_history($_SESSION['user_id'], $_POST['activity_id'], $connect); //bind parameters to the database.php
-
-?>
+    //fetch the group chat history of a certain activity when a user open the group chat dialogue box
+    
+    include('chatdatabase_connection.php');
+    
+    session_start(); //get session variable
+    
+    echo fetch_activity_chat_history($_SESSION['user_id'], $_POST['activity_id'], $connect); //bind parameters to the database.php
+    
+    ?>
