6f08bb053d59f97cc7f5e77b750c644f66408906
ckwong2001 <62646998+ckwong2001@users.noreply.github.com>
2020-05-17 12:59:32 +0800

    Update reset-request.inc.php

--- includes/reset-request.inc.php
+++ includes/reset-request.inc.php
@@ -1,3 +1,5 @@
+<!--backend code when one submit request for resetting password-->
+
 <?php
 
 if (isset($_POST["reset-request-submit"])) {
@@ -5,12 +7,13 @@ if (isset($_POST["reset-request-submit"])) {
 $selector = bin2hex(random_bytes(8));
 $token = random_bytes(32);
 
+    //create a unique url for each password reset request 
 $url = "localhost/habitracker/create-new-password.php?selector=" . $selector . "&validator=" . bin2hex($token);
 $expires = date("U") + 1800; //30 min from now
 
 require 'dbh.inc.php'; //my database connection file
 
-$userEmail = $_POST["email"]; //"email" is the attribute in our form
+$userEmail = $_POST["email"]; 
 
 $sql = "DELETE FROM pwdreset WHERE pwdResetEmail=?;";  //make sure no existing token of the same user in database
 $stmt = mysqli_stmt_init($conn);
@@ -18,10 +21,11 @@ if (!mysqli_stmt_prepare($stmt, $sql)){
     echo "There was an error!";
     exit();
 } else {
-    mysqli_stmt_bind_param($stmt, "s", $userEmail); // replace the ? by the data input by user, which is $userEmail
+    mysqli_stmt_bind_param($stmt, "s", $userEmail); 
     mysqli_stmt_execute($stmt);
 }
 
+    //update the database about the reset request 
 $sql = "INSERT INTO pwdreset (pwdResetEmail, pwdResetSelector, pwdResetToken, pwdResetExpires) VALUES (?, ?, ?, ?);";
 
 $stmt = mysqli_stmt_init($conn);
@@ -37,7 +41,7 @@ if (!mysqli_stmt_prepare($stmt, $sql)){
 mysqli_stmt_close($stmt);
 mysqli_close($conn);
 
-
+//send email to users for resetting their password automatically after they request 
 $to = $userEmail; //send email to users
 
 $subject = 'Reset your password for habitracker';
@@ -47,12 +51,6 @@ $message .= '<p>The password reset is only valid for the next 30 minutes.</br>';
 $message .= '<p>Here is your password reset link: </br>'; // .= means to continue the previous message
 $message .= '<a href="' . $url . '">' . $url . '</a></p></br>';
 $message .= '<p>Please send an email to noreply-habitracker@gmail.com if you have any queries.';
-//$message .= '<a href="localhost/loginsystem/create-new-password.php">CreateNewPassword</a></p>';
-
-//$headers = "From: habitracker <habitracker@gmail.com>\r\n"; //email should be set up 
-//$headers .= "Reply-To: <habitracker@gmail.com>\r\n";
-//$headers .= "Content-type: text/html\r\n";
-
 
 require_once('PHPMailer-5.2-stable/PHPMailerAutoload.php');
 
@@ -65,14 +63,11 @@ $mail->Port = '465';
 $mail->isHTML();
 $mail->Username = 'noreply.habitracker@gmail.com';//your own email address
 $mail->Password = 'csci3100';
-//$mail->SetFrom('no-reply@howcode.org');
 $mail->Subject = $subject;;
 $mail->Body = $message;
 $mail->AddAddress($to);
 
 $mail->Send();
-
-//mail($to, $subject, $messsage, $headers);     //need a mail server to run this
 
 header("Location: ../reset-password.php?reset=success");
 
