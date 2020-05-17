3c8fe56e332c7fc8c076e14259019b499219ff94
ckwong2001 <62646998+ckwong2001@users.noreply.github.com>
2020-05-17 11:32:45 +0800

    Update profile_edit.php

--- profile_edit.php
+++ profile_edit.php
@@ -1,3 +1,5 @@
+<!--display users' profile information when one clicks into the edit profile page-->
+
 <?php
     require 'header.php';
 ?>
@@ -23,6 +25,7 @@
         exit();
     }
 
+        //retrieve user's personal information from the database 
     require 'db_key.php';
     $conn = connect_db();
     $sql = "Select * from login Where username = '$username'";
@@ -30,15 +33,15 @@
     $row = $search_result->fetch_assoc();
     $id = $row['user_id'];
 ?>
-
-
-    
-
         <div class="loginbox">
 
         <div class="image-container">
 
     <?php
+        
+    //display one's profile picture in the correct format if one has updated his picture before
+    //display a default profile instead if one has not updated his picture before
+    
     echo "<div>";
     if ($row['image_status'] == 0){
         echo "<img src='upload_image/profile".$id.".jpg' height ='150'>";
@@ -54,7 +57,8 @@
 
     ?>
     </div>
-     
+    
+            <!-- display one's personal information in the correct format while user is editing their information-->
             <form action="includes/profile_edit.inc.php" method="post"> 
             <div class="edit-display">
             <tr>
@@ -81,7 +85,7 @@
             ?> placeholder="Enter your welcoming message" <?php } 
                 ?> name="welcome_message"></br>
 
-            
+        <!--allow user to submit the edit profile request to the backend application>    
             <button class="finish_edit_profile" type="submit" name="finish-edit-profile-submit">Finish editing</button>
             </div>
             </form>
